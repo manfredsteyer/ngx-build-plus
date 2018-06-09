b2928b4118224b9063e2c15ffca9e5a787b075bd
ManfredSteyer <manfred.steyer@gmx.net>
2018-06-09 23:33:24 +0200

    removed js and d.ts

--- lib/src/plus/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { BrowserBuilder } from '@angular-devkit/build-angular';
-import { Path, virtualFs } from '@angular-devkit/core';
-import * as fs from 'fs';
-import { PlusBuilderSchema } from './schema';
-export declare class PlusBuilder extends BrowserBuilder {
-    buildWebpackConfig(root: Path, projectRoot: Path, host: virtualFs.Host<fs.Stats>, options: PlusBuilderSchema): any;
-}
-export default PlusBuilder;
