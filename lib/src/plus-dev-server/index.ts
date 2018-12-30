6d8428929361922147df8da566268af55a22f043
ManfredSteyer <manfred.steyer@gmx.net>
2018-12-31 00:06:19 +0100

    PR #49, #45, #44, #43, #42

--- lib/src/plus-dev-server/index.ts
+++ lib/src/plus-dev-server/index.ts
@@ -1,10 +1,15 @@
 import { Path, getSystemPath, virtualFs } from '@angular-devkit/core';
 import * as path from 'path';
 import * as fs from 'fs';
-import { ConfigHookFn } from '../ext/hook';
+import { ConfigHookFn, Plugin } from '../ext/hook';
 import { loadHook } from '../ext/load-hook';
 
-import { DevServerBuilder as DevServerBuilderBase, BrowserBuilderSchema as BrowserBuilderSchemaBase   } from '@angular-devkit/build-angular';
+import { DevServerBuilder as DevServerBuilderBase, BrowserBuilderSchema as BrowserBuilderSchemaBase, DevServerBuilderOptions   } from '@angular-devkit/build-angular';
+import { BuildEvent } from '@angular-devkit/architect';
+
+import { Observable } from '@angular-devkit/schematics/node_modules/rxjs';
+import { tap } from 'rxjs/operators';
+
 const webpackMerge = require('webpack-merge');
 
 export interface BrowserBuilderSchema extends BrowserBuilderSchemaBase {
@@ -12,25 +17,24 @@ export interface BrowserBuilderSchema extends BrowserBuilderSchemaBase {
   singleBundle: boolean;
   bundleStyles: boolean;
   configHook: string;
+  plugin: string;
 }
 
 export class PlusDevServerBuilder extends DevServerBuilderBase {
 
   private localOptions: any;
 
-  run(builderConfig: any): any {
-        this.localOptions = builderConfig.options;
-    return super.run(builderConfig);
-  }
+  buildWebpackConfig(
+    root: Path,
+    projectRoot: Path,
+    host: virtualFs.Host<fs.Stats>,
+    options: BrowserBuilderSchema,
+  ) {
 
-  buildWebpackConfig(root: Path, projectRoot: Path, host: virtualFs.Host<fs.Stats>, browserOptions: BrowserBuilderSchema): any {
-
-    let config = super.buildWebpackConfig(root, projectRoot, host, browserOptions);
+    let config = super.buildWebpackConfig(root, projectRoot, host, options);
 
     if (this.localOptions.singleBundle) {
       delete config.entry.polyfills;
-
-      delete config.entry.styles;
       delete config.optimization.runtimeChunk;
       delete config.optimization.splitChunks;
     }
@@ -45,12 +49,43 @@ export class PlusDevServerBuilder extends DevServerBuilderBase {
       config = webpackMerge([config, additionalConfig]);
     }
 
+    let plugin: Plugin | null = null;
+    if (this.localOptions.plugin) {
+      plugin = loadHook<Plugin>(this.localOptions.plugin);
+    }
+
+    if (plugin && plugin.config) {
+      config = plugin.config(config);
+    }
+
     if (this.localOptions.configHook) {
       const hook = loadHook<ConfigHookFn>(this.localOptions.configHook);
       config = hook(config);
     }
 
     return config;
+  }
+
+  run(builderConfig: any): Observable<BuildEvent> {
+    
+    this.localOptions = builderConfig.options;
+    let plugin: Plugin | null = null;
+    if (builderConfig.options.plugin) {
+      plugin = loadHook<Plugin>(builderConfig.options.plugin);
+    }
+
+    if (plugin && plugin.pre) {
+      plugin.pre(builderConfig);
+    }
+
+    return super.run(builderConfig).pipe(
+      tap(_ => {
+        if (plugin && plugin.post) {
+          plugin.post(builderConfig);
+        }
+      })
+    );
+
   }
 
   
