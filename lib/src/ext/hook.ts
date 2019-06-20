8b31fbb54d34c0f14d4a5cc5cadc9628c960ba88
CSchulz <CSchulz@users.noreply.github.com>
2019-06-20 10:49:06 +0200

    add preConfig hook

--- lib/src/ext/hook.ts
+++ lib/src/ext/hook.ts
@@ -4,6 +4,7 @@ import { BrowserBuilderSchema } from "src/plus-dev-server";
 export type ConfigHookFn = (cfg: object) => object;
 
 export type Plugin = {
+    preConfig?(builderConfig: BuilderConfiguration<BrowserBuilderSchema>): void;
     config?(cfg: object): object;
     pre?(builderConfig: BuilderConfiguration<BrowserBuilderSchema>): void;
     post?(builderConfig: BuilderConfiguration<BrowserBuilderSchema>): void
