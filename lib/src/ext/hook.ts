3cda922d7f3059b1982857c6d9623819abf69b5f
Christian Schulz <christian.schulz@openknowledge.de>
2019-06-20 11:21:28 +0200

    add some documentation

--- lib/src/ext/hook.ts
+++ lib/src/ext/hook.ts
@@ -3,7 +3,18 @@ import { BuilderConfiguration } from "@angular-devkit/architect";
 export type ConfigHookFn = (cfg: object) => object;
 
 export type Plugin<O, N> = {
+    /**
+     * Called before webpack configuration is created
+     * Receives the normalized and merged build configuration over multiple targets
+     *
+     * @param normalizedBuilderConfig
+     */
     preConfig?(normalizedBuilderConfig: N): void;
+    /**
+     * Called after webpack configuration was created
+     *
+     * @param cfg
+     */
     config?(cfg: object): object;
     pre?(builderConfig: BuilderConfiguration<O>): void;
     post?(builderConfig: BuilderConfiguration<O>): void
